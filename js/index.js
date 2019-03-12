f79ec419387d38f51a725452876f59943407fe40
Ilich Arredondo <ilich.arredondo@amplemind.com>
2019-03-12 15:28:22 -0600

    Optimized slider instanciation

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ const tabsElement = document.querySelector('.canchas .tabs');
 var canchasTab = M.Tabs.init(tabsElement, {
   duration: 275
 });
-
-var slider = tns({
-  container: '#canchas-slider1',
+// Sliders instancies and declarations
+const sliders = [];
+const options = {
   items: 1,
   slideBy: 'page',
   autoplayButtonOutput: false,
@@ -13,34 +13,9 @@ var slider = tns({
   controls: false,
   nav: false,
   mouseDrag: true
-});
-var slider = tns({
-  container: '#canchas-slider2',
-  items: 1,
-  slideBy: 'page',
-  autoplayButtonOutput: false,
-  autoplay: true,
-  controls: false,
-  nav: false,
-  mouseDrag: true
-});
-var slider = tns({
-  container: '#canchas-slider3',
-  items: 1,
-  slideBy: 'page',
-  autoplayButtonOutput: false,
-  autoplay: true,
-  controls: false,
-  nav: false,
-  mouseDrag: true
-});
-var slider = tns({
-  container: '#canchas-slider4',
-  items: 1,
-  slideBy: 'page',
-  autoplayButtonOutput: false,
-  autoplay: true,
-  controls: false,
-  nav: false,
-  mouseDrag: true
+};
+document.querySelectorAll('[id*="canchas-slider"]').forEach(e => {
+  const containerObj = { container: `#${e.id}` };
+  let sliderInstance = tns(Object.assign(containerObj, options));
+  sliders.push(sliderInstance);
 });
